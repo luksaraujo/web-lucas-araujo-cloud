5916b76291a6cb33d4124a3da21d2bddb532a6f4
luksaraujo <lucasarasouza@hotmail.com>
2024-07-14 15:39:43 -0300

    [feat]: Referencing Lucas Araujo's home page section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/Header";
+import LucasAraujo from "../../components/HomeLucasAraujo";
 
 const HomePage = () => {
     return(
         <>
             <Header />
+            <LucasAraujo />
         </>
     );
 };
