3160a0825f125c8c8c04f818e6f6ab1d097470c9
luksaraujo <lucasarasouza@hotmail.com>
2024-07-15 18:23:01 -0300

    [feat]: Adding the description on Lucas Araujo's section on home page

--- src/components/HomeLucasAraujo/index.jsx
+++ src/components/HomeLucasAraujo/index.jsx
@@ -1,10 +1,29 @@
 import "./index.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BannerLucas from "../../assets/banner-lucas.jpg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const LucasAraujo = () => {
+    const azureCertificationsAmount = 10;
+
+    const [age, setAge] = useState(0);
+
+    useEffect(() => {
+        const birthDate = '1999-12-07';
+        const calculateAge = (birthDate) => {
+            const today = new Date();
+            const birthDateObj = new Date(birthDate);
+            let age = today.getFullYear() - birthDateObj.getFullYear();
+            const monthDifference = today.getMonth() - birthDateObj.getMonth();
+            if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDateObj.getDate())) {
+                age--;
+            }
+            return age;
+        };
+        setAge(calculateAge(birthDate));
+    }, []);
+
     return(
         <section id="lucas_araujo">
             <div className="shape"></div>
@@ -26,7 +45,22 @@ const LucasAraujo = () => {
                 </div>
 
                 <p className="description">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore labore aliquid consectetur hic est, illum totam laboriosam, sit cum nobis ratione molestiae debitis, earum dicta vero nisi tempore repellendus praesentium!
+                    Aos {age} anos, Lucas Araújo é hoje considerado <b>um dos melhores
+                    especialistas técnicos</b> e de instrução <b>em tecnologias Microsoft</b>
+                    do Brasil.<br/><br/>Com mais de {azureCertificationsAmount} certificações
+                    em todos os níveis de conhecimento do Microsoft Azure, Lucas <b>ensina de
+                    uma maneira única</b> aplicando a técnica DDL (Depth Detailed Learning),
+                    que foi trazida dos Estados Unidos e aprimorada pelo especialista
+                    brasileiro, focando no <b>ensino profundo e detalhado dos conteúdos
+                    abordados</b>, onde a teoria se une à prática e <b>capacita os melhores
+                    profissionais do mercado</b>.<br/><br/><b>O DIFERENCIAL</b><br/><br/>
+                    Lucas Araújo é um mestre no ensino da computação em nuvem, unindo sua
+                    experiência prática em projetos das mais variadas complexidades e tamanhos,
+                    realizados para inúmeras multinacionais, com a arte de ensinar, alcança
+                    resultados excepcionais. Sua vasta experiência, somada à técnica DDL e
+                    a sua cuidadosa atenção aos detalhes e a cada aluno em particular garantem
+                    que cada indivíduo alcance o seu melhor potencial, tornando-se um profissional
+                    devidamente qualificado.
                 </p>
                 
             </div>
