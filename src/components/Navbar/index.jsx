88313e3f427efdf070005b08ea81d863d9672c06
luksaraujo <lucasarasouza@hotmail.com>
2024-07-16 18:36:09 -0300

    [feat]: Adding responsiviness to Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,16 @@
 import "./index.css";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import LogoFull from "../LogoFull";
+import { FaBars, FaX } from "react-icons/fa6";
 
 const Navbar = () => {
+    const [isMobileNavbarActive, setIsMobileNavbarActive] = useState(false);
+
+    const handleMobileNavbar = () => {
+        isMobileNavbarActive === true ? setIsMobileNavbarActive(false) : setIsMobileNavbarActive(true);
+    }
+
     // const [isNavbarActive, setIsNavbarActive] = useState("navbar_menu");
     // const [navbarToggleIcon, setNavbarToggleIcon] = useState("navbar_toggler");
     // const navbarToggle = () => {
@@ -16,7 +23,7 @@ const Navbar = () => {
                 <Link to="/" className="navbar_link"><LogoFull /></Link>
             </div>
 
-            <ul className="navbar_menu">
+            <ul className="navbar_menu" id="navbar_menu_desktop">
                 <li className="navbar_item"><Link to="/" className="navbar_link">Home</Link></li>
                 <li className="navbar_item"><Link to="/lucas-araujo" className="navbar_link">Lucas Araújo</Link></li>
                 <li className="navbar_item"><Link to="/cursos-e-treinamentos" className="navbar_link">Cursos e Treinamentos</Link></li>
@@ -26,18 +33,13 @@ const Navbar = () => {
                 <button class="navbar_schedule_btn">AGENDE UM HORÁRIO</button>
             </div>
 
+            <div id="navbar_mobile_menu" onClick={handleMobileNavbar}>
+                {isMobileNavbarActive ? <FaX class="navbar_mobile_menu_icon" /> : <FaBars class="navbar_mobile_menu_icon" />}
+            </div>
+
             {/* <a href="https://app-la-cloud-prod-brso-01.azurewebsites.net" className="navbar_brand">LA Cloud</a>
-            <ul className={isNavbarActive}>
-                <li className="navbar_item"><a href="https://app-la-cloud-prod-brso-01.azurewebsites.net" className="navbar_link">Home</a></li>
-                <li className="navbar_item"><a href="https://app-la-cloud-prod-brso-01.azurewebsites.net" className="navbar_link">Lucas Araújo</a></li>
-                <li className="navbar_item"><a href="https://app-la-cloud-prod-brso-01.azurewebsites.net" className="navbar_link">Cursos e Treinamentos</a></li>
-                <li className="navbar_item"><a href="https://app-la-cloud-prod-brso-01.azurewebsites.net" className="navbar_link">Agende um horário comigo</a></li>
-            </ul>
-            <div onClick={navbarToggle} className={navbarToggleIcon}>
-                <div id="line_01"></div>
-                <div id="line_02"></div>
-                <div id="line_03"></div>
-            </div> */}
+            <ul className={isNavbarActive}></ul>
+            <div onClick={navbarToggle} className={navbarToggleIcon}></div> */}
         </nav>
     );
 };
