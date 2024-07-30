3c2d9c2f0d25d4991bb3d49ed39c426ce34b73ec
luksaraujo <lucasarasouza@hotmail.com>
2024-07-29 22:15:03 -0300

    [feat]: Refactoring home page structure

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import LucasAraujo from "../../components/HomeLucasAraujo";
 import ComunidadeCloudAlliance from "../../components/HomeComunidade";
+import HomeCertificacaoEm4Semanas from "../../components/HomeCertificacaoEm4Semanas";
+import HomeGarantiaCertificado from "../../components/HomeGarantiaCertificado";
 
 const HomePage = () => {
     return(
         <>
             <div id="content">
-                <ComunidadeCloudAlliance />
+                <HomeCertificacaoEm4Semanas />
+                <HomeGarantiaCertificado />
                 <LucasAraujo />
+                {/*<ComunidadeCloudAlliance />*/}
             </div>
         </>
     );
